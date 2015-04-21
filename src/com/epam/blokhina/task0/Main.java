1c06d53f60ce0bbd3bd664eab53f7e8f261d3560
yevheniia_blokhina <yevheniia_blokhina@epam.com>
2015-04-21 17:43:34 +0300

    Initial commit

---
+++ src/com/epam/blokhina/task0/Main.java
@@ -0,0 +1,16 @@
+package com.epam.blokhina.task0;
+
+/**
+ * Created by Yevheniia_Blokhina on 4/21/2015.
+ */
+public class Main {
+
+    Patient p1 = new Patient("Lapot`", "Vasya", "Grigorievich");
+    Patient p2 = new Patient("Lal", "Vanys", "Captoa");
+    Patient p3 = new Patient("Lalasd", "Ana", "Rasa", "Kharkiv, ul.Takaya-to 0", 1002, "chickenpox");
+    Patient p4 = new Patient("Mu", "Mala", "Captoa", "ul.8Marta", 231, "flu");
+    Patient p5 = new Patient("Capa", "Vaasdnys", "CaptoRRa", "ul.9Marta", 3212, "flu");
+    Patient p6 = new Patient("Lama", "Saha", "Serheevna", "ul.9Marta", 3212, "cold");
+    Patient p7 = new Patient("Rapun", "Katya", "Igorevna", "ul.9Marta", 3212, "measles");
+
+}
---
+++ src/com/epam/blokhina/task0/Patient.java
@@ -0,0 +1,95 @@
+package com.epam.blokhina.task0;
+
+/**
+ * Created by Yevheniia_Blokhina on 4/21/2015.
+ */
+//3. Patient: id, Фамилия, Имя, Отчество, Адрес, Телефон, Номер медицинской карты, Диагноз.
+//
+//        Создать массив объектов. Вывести:
+//
+//        a) список пациентов, имеющих данный диагноз;
+//
+//        b) список пациентов, номер медицинской карты которых находится в за-данном интервале.
+public class Patient {
+
+    private static int count = 0;
+    private int id;
+    private String lastName;
+    private String firstName;
+    private String patronymic;
+    private String address;
+    private int medicalNumber;
+    private String diagnosis;
+
+    public Patient(String lastName, String firstName, String patronymic, String address, int medicalNumber, String diagnosis) {
+        id = ++count;
+        setLastName(lastName);
+        setFirstName(firstName);
+        setPatronymic(patronymic);
+        setAddress(address);
+        setMedicalNumber(medicalNumber);
+        setDiagnosis(diagnosis);
+    }
+
+    public Patient(String lastName, String firstName, String patronymic) {
+        setLastName(lastName);
+        setFirstName(firstName);
+        setPatronymic(patronymic);
+        address = "";
+        id = ++count;
+        medicalNumber = 100+id;
+        diagnosis = "unknown disease";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getMedicalNumber() {
+        return medicalNumber;
+    }
+
+    public void setMedicalNumber(int medicalNumber) {
+        this.medicalNumber = medicalNumber;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+}
